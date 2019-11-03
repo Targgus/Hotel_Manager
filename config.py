e7107d1cf8811335f10f4eb58b489ff0f701c87c
Targgus <jlhenson13@gmail.com>
2019-11-02 21:59:09 -0600

    Adding Comments

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
 class Config():
+    # grabs current base directory name
     basedir = os.path.abspath(os.path.dirname(__file__))
+    # places sqlite database in this directory
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
