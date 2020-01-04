aec074d0c98def4d0e0912ccc48f9044739bff0f
Targgus <jlhenson13@gmail.com>
2020-01-04 15:26:21 -0700

    Changed port number

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.debug = True
-    # app.run(host='127.0.0.1', port=8080, debug=True)
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
+    # app.run()
